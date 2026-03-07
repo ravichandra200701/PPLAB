9aad04d1dcc7e977ddeddb2b3ec615a5ce21ca0a
Ravichandra <gunjaravichandra@gmail.com>
2026-03-07 21:07:13 +0530

    Refactor length conversion using match-case structure

--- ex14.py
+++ ex14.py
@@ -1,36 +1,23 @@
-f=int(input('Enter length in feet:'))
+li = ['inches','yards','miles','millimetres','centi_meters','meters','kilo_meters']
+val = [12, 1/3, 1/1580, 304.8, 30.48, 0.3048, 0.0003048]
 while True:
-    print('1. Inches')
-    print('2. Yards')
-    print('3. Miles')
-    print('4. Millimeters')
-    print('5. Centimeters')
-    print('6. meters')
-    print('7. Kilometers')
-    print('8. Exit')
-    a=int(input('Enter your choice:'))
-    while True:
-        if a<=8:
-            break
-        else:
-            a=int(input("Enter correct number:"))
-            if a<=8:
-                break
-    if a==1:
-        r=f*12
-    elif a==2:
-        r=f/3.0
-    elif a==3:
-        r=f/5280.0
-    elif a==4:
-        r=f*304.8
-    elif a==5:
-        r=f*30.48
-    elif a==6:
-        r=f*0.3048
-    elif a==7:
-        r=f*0.0003048
-    elif a==8:
-        break
-    print('The required result=',r)
+    n =int(input("enter length in feet: "))
+    conversion = int(input("1.inches\n2.yards\n3.miles\n4.millimetres\n5.centimeters\n6.meters,\n7.kilometers\nenter the conversion: "))
 
+    match conversion:
+        case 1:
+            print(f"Length from feet to {li[0]}:{n*val[0]}")
+        case 2:
+            print(f"Length from feet to {li[1]}:{n*val[1]}")
+        case 3:
+            print(f"Length from feet to {li[2]}:{n*val[2]}")
+        case 4:
+            print(f"Length from feet to {li[3]}:{n*val[3]}")
+        case 5:
+            print(f"Length from feet to {li[4]}:{n*val[4]}")
+        case 6:
+            print(f"Length from feet to {li[5]}:{n*val[5]}")
+        case 7:
+            print(f"Length from feet to {li[6]}:{n*val[6]}")
+        case _:
+            break
